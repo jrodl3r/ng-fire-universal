05278e77ab9ecd854e5c6b275d4c761b7e9b141e
John Rodler <jrodl3r@gmail.com>
2019-07-30 09:39:04 -0700

    tweak avatar background logic

--- src/app/components/_user/home/home.component.ts
+++ src/app/components/_user/home/home.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../../../services/auth.service';
 import { Component, Inject, forwardRef } from '@angular/core';
 
 import { UserService } from 'src/app/services/user.service';
@@ -10,7 +11,13 @@ import { UserService } from 'src/app/services/user.service';
 export class HomeComponent {
 
   constructor(
+    @Inject(forwardRef(() => AuthService)) public auth: AuthService,
     @Inject(forwardRef(() => UserService)) public user: UserService
   ) { }
+
+  signOut(event: Event) {
+    event.preventDefault();
+    this.auth.signOut();
+  }
 
 }
